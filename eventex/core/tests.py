aceaf1cef62c6d1d475ef8e96597cd22e238b9e1
amenezes <alexandre.fmenezes@gmail.com>
2019-06-13 10:25:07 -0300

    inclusao da pagina de inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class EventexCoreTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_home(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_eventex_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
